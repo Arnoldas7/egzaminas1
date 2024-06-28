06dd4b7cda47b2a7f260d5bbb7fbddca5418d896
Arnoldas7 <arnoldmldc@gmail.com>
2024-06-28 12:04:00 +0300

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,7 +23,7 @@ class PostController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'content' => 'required|string',
-            ''=> '',
+          
         ]);
 
         Post::create($request->only(['title', 'content']));
